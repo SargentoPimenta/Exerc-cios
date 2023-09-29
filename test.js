199751acd5f0ce638647b40f04d9a877e673d9e7
Marcus Brazão <141243829+SargentoPimenta@users.noreply.github.com>
2023-09-29 00:27:26 -0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-
-const { removeDuplicates } = require('./remove_duplicates');
-
-const inputList = [1, 2, 2, 3, 4, 4, 5];
-const uniqueList = removeDuplicates(inputList);
-
-console.log(uniqueList);
